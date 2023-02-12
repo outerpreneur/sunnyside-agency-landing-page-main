c7a8ec18a978e820754421c36daa157875af8fd1
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-02-12 11:36:50 +0100

    menu mobile event

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const menuMobileButton = document.querySelector('.menu-icon-mobile')
-
 const menuMobile = document.querySelector('.menu-mobile')
-
 
 menuMobileButton.addEventListener('click', () => {
     menuMobile.classList.toggle("hidden")
